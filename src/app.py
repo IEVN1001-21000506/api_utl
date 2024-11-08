465cae8e0c34d13ce3500a52a5c2af558539b42c
Cesar Andres Ramirez Garcia <andgg12345@gmail.com>
2024-11-07 20:47:38 -0600

    commit final servicios terminados

--- src/app.py
+++ src/app.py
@@ -58,26 +58,66 @@ def leer_alumno(mat):
             return jsonify({"alumno": None, "mensaje": "Alumno no encontrado", "exito": False}), 404
     except Exception as ex:
         return jsonify({"message": f"error {ex}", "exito": False}), 500
-
-@app.route('/alumnos', methods=['POST'])
+   
+ 
+@app.route('/alumnos',methods=['POST'])
 def registrar_alumno():
     try:
-        alumno:leer_alumnobd(request_json['matrricula'])
+        alumno=leer_alumno_bd(request.json['matricula'])
         if alumno !=None:
-            return jsonify({"mensaje": "Alumno ya existe, no se puede ducplicar", "exito": False})
-            else:
-                cursos=con-connection.cursor()
-                sql="""insert into alumnos (matricula, nombre, apaterno, amaterno, correo)
-                values ({0}, {1}, {2}, {3}, {4})""".format(request.json['matricula'],
-                request.json['nombre'], request.json['apaterno'],request.json['amaterno'],
-                request.json['correo'])
-            cursor.execute(sql)
-            con.connection.commit()
-            return jsonify({"mensaje": "Alumno registrado", "exito":True})
-
+            return jsonify({"mensaje": "Alumno ya Existe, No se Puede Duplicar", "exito": False})
+        else:
+            cursor = con.connection.cursor()
+            sql = """INSERT INTO alumnos (matricula, nombre, apaterno, amaterno, correo) VALUES ('{0}', '{1}', '{2}', '{3}', '{4}')""".format(
+            request.json['matricula'],
+            request.json['nombre'],
+            request.json['apaterno'],
+            request.json['amaterno'],
+            request.json['correo'])
+        cursor.execute(sql)
+        con.connection.commit()
+        return jsonify({"mensaje":"Alumno registrado", "exito":True})
+   
     except Exception as ex:
-        return jsonify({"mensaje": "Error", "exito":True})
-        pass
+        return jsonify ({"mensaje":"Error","exito":False})
+   
+ 
+@app.route('/alumnos/<mat>', methods=['PUT'])
+def actualizar_curso(mat):
+    #if (validar_matricula(mat) and validar_nombre(request.json['nombre']) and validar_apaterno(request.json['apaterno'])):
+        try:
+            alumno = leer_alumno_bd(mat)
+            if alumno != None:
+                cursor = con.connection.cursor()
+                sql = """UPDATE alumnos SET nombre = '{0}', apaterno = '{1}', amaterno='{2}', correo='{3}'
+                WHERE matricula = {4}""".format(request.json['nombre'], request.json['apaterno'], request.json['amaterno'],request.json['correo'], mat)
+                cursor.execute(sql)
+                con.connection.commit()  # Confirma la acción de actualización.
+                return jsonify({'mensaje': "Alumno actualizado.", 'exito': True})
+            else:
+                return jsonify({'mensaje': "Alumno no encontrado.", 'exito': False})
+        except Exception as ex:
+            return jsonify({'mensaje': "Error {0} ".format(ex), 'exito': False})
+    #else:
+     #   return jsonify({'mensaje': "Parámetros inválidos...", 'exito': False})
+ 
+ 
+@app.route('/alumnos/<mat>', methods=['DELETE'])
+def eliminar_curso(mat):
+    try:
+        alumno = leer_alumno_bd(mat)
+        if alumno != None:
+            cursor = con.connection.cursor()
+            sql = "DELETE FROM alumnos WHERE matricula = {0}".format(mat)
+            cursor.execute(sql)
+            con.connection.commit()  # Confirma la acción de eliminación.
+            return jsonify({'mensaje': "Alumno eliminado.", 'exito': True})
+        else:
+            return jsonify({'mensaje': "Alumno no encontrado.", 'exito': False})
+    except Exception as ex:
+        return jsonify({'mensaje': "Error", 'exito': False})
+ 
+ 
  
 def pagina_no_encontrada(error):
     return "<h1>Pagina no encontrada</h1>", 404
@@ -85,4 +125,4 @@ def pagina_no_encontrada(error):
 if __name__ == "__main__":
     app.config.from_object(config['development'])
     app.register_error_handler(404, pagina_no_encontrada)
-    app.run(host='0.0.0.0', port=5001)
+    app.run(host='0.0.0.0', port=5000)
